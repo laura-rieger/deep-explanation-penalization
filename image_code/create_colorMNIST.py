3e5f10075d5285c3b8fa63835f54dcbaf9dd136a
Laura Rieger <lauri@compute.dtu.dk>
2019-05-30 20:02:27 +0200

    results on colormnist

--- image_code/create_colorMNIST.py
+++ image_code/create_colorMNIST.py
@@ -1,21 +1,40 @@
 import torch
 import torchvision
 import torchvision.datasets as datasets
-import matplotliblib.pyplot as plt
+import sys
 import numpy as np
 import torch.utils.data as utils
+from colour import Color
+red = Color("red")
+colors = list(red.range_to(Color("blue"),10))
+colors = [x.get_rgb() for x in colors]
 from os.path import join as oj
 mnist_trainset = datasets.MNIST(root='../data', train=True, download=True, transform=None)
 color_x = np.zeros((60000, 3, 28, 28))
-color_x[np.where((mnist_trainset.train_labels <5)), 0 ] =  mnist_trainset.data[np.where((mnist_trainset.train_labels <5))]
-color_x[np.where((mnist_trainset.train_labels >=5)),1] =  mnist_trainset.data[np.where((mnist_trainset.train_labels >=5))]
+
+
+for i in range(10):
+    color_x[np.where((mnist_trainset.train_labels ==i)) ] = (mnist_trainset.data[np.where((mnist_trainset.train_labels ==i))].numpy().astype(np.float32)[:, np.newaxis, :,:]*np.asarray(colors[i])[None, :, None, None])
+
+
+
+
 color_y = mnist_trainset.train_labels.numpy().copy()
+
+color_x /=color_x.max()
+color_x = color_x*2 -1
 np.save(oj("../data/ColorMNIST", "train_x.npy"), color_x)
 np.save(oj("../data/ColorMNIST", "train_y.npy"), color_y)
+
+
 mnist_trainset = datasets.MNIST(root='../data', train=False, download=True, transform=None)
 color_x = np.zeros((10000, 3, 28, 28))
-color_x[np.where((mnist_trainset.train_labels >=5)), 0 ] =  mnist_trainset.data[np.where((mnist_trainset.train_labels <5))]
-color_x[np.where((mnist_trainset.train_labels <5)),1] =  mnist_trainset.data[np.where((mnist_trainset.train_labels >=5))]
+for i in range(10):
+    color_x[np.where((mnist_trainset.train_labels ==i)) ] = (mnist_trainset.data[np.where((mnist_trainset.train_labels ==i))].numpy().astype(np.float32)[:, np.newaxis, :,:]*np.asarray(colors[9-i])[None, :, None, None])
 color_y = mnist_trainset.train_labels.numpy().copy()
-np.save(oj("../data/ColorMNIST", "test_x.npy"), color_x)
+color_x /=color_x.max()
+color_x = color_x*2 -1
+np.save(oj("../data/ColorMNIST", "test_x.npy"),  color_x)
 np.save(oj("../data/ColorMNIST", "test_y.npy"), color_y)
+np.save(oj("../data/ColorMNIST", "test_y_color.npy"), 9-color_y)
+print("Saved color MNIST")
---
+++ image_code/params_save.py
@@ -0,0 +1,22 @@
+
+import numpy as np
+from numpy.random import randint
+
+class S:   
+    def __init__(self, num_epochs):
+        # accs / losses
+        self.losses_train = np.zeros(num_epochs) # training loss curve (should be plotted against p.its)
+        self.losses_test = np.zeros(num_epochs)  # testing loss curve (should be plotted against p.its)
+        self.accs_train = np.zeros(num_epochs)   # training acc curve (should be plotted against p.its)               
+        self.accs_test = np.zeros(num_epochs)    # testing acc curve (should be plotted against p.its)
+                    
+        self.cd = np.zeros(num_epochs)    # testing acc curve (should be plotted against p.its)
+        self.model_weights = None
+        self.regularizer_rate = 0
+        self.num_blobs = 0
+
+    
+    # dictionary of everything but weights
+    def _dict(self):
+        return {attr: val for (attr, val) in vars(self).items()}
+        
